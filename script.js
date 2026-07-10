875a084838a5b8b870500d91028ced3370a303bd
cse25-421 <cse25-421@thuto.bac.ac.bw>
2026-07-10 20:24:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,26 +8,6 @@ function searchTopic() {
     }
 }
 
-function checkQuiz(event) {
-    event.preventDefault();
-
-    var score = 0;
-    var totalQuestions = 15;
-
-    for (var i = 1; i <= totalQuestions; i++) {
-        var answer = document.querySelector('input[name="q' + i + '"]:checked');
-
-        if (answer && answer.value == "correct") {
-            score++;
-        }
-
-        if (answer && answer.name == "q2" && answer.nextSibling.textContent.trim().charAt(0) == "0") {
-            score++;
-        }
-    }
-
-    alert("You scored " + score + " out of " + totalQuestions);
-}
 
 function sendFeedback(event) {
     event.preventDefault();
